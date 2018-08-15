97317f6a9c490fab9bd64112c2b0c56d67b59513
EvanKDodge <evan.k.dodge@gmail.com>
2018-08-14 18:05:56 -0700

    Added ELB. Now outputs elb dns name instead of ip.

--- ch2/main.tf
+++ ch2/main.tf
@@ -5,10 +5,9 @@ variable "server_port" {
     default = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
-
 
 provider "aws" {
     region = "us-west-1"
@@ -44,6 +43,10 @@ resource "aws_security_group" "instance" {
 resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
     availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+    load_balancers = ["${aws_elb.example.name}"]
+    health_check_type = "ELB"
+
     min_size = 2
     max_size = 10
 
@@ -51,5 +54,44 @@ resource "aws_autoscaling_group" "example" {
         key = "Name"
         value = "terraform-asg-example"
         propagate_at_launch = true
+    }
+}
+
+resource "aws_elb" "example" {
+    name = "terraform-asg-example"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = "${var.server_port}"
+        instance_protocol = "http"
+    }
+
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target = "HTTP:${var.server_port}"
+    }
+}
+
+resource "aws_security_group" "elb" {
+    name = "terraform_example_elb"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
